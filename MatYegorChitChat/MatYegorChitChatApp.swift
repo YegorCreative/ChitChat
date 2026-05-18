e0a10a1fc216ce463b4187fd0516cc568b0ada3b
YegorCreative <aleqsandregor@gmail.com>
2026-05-18 17:19:53 -0500

    Initial Commit

---
+++ MatYegorChitChat/MatYegorChitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatYegorChitChatApp.swift
+//  MatYegorChitChat
+//
+//  Created by Yegor Hambaryan on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct MatYegorChitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
